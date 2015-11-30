9f1ffa2d2c7f954bd111c1dd0de2a8be31dceaf8
wa <admin@logicplace.com>
2015-11-30 06:44:48 -0800

    this stuff idk

--- datamgr.py
+++ datamgr.py
@@ -14,6 +14,9 @@ options.add_argument("--generalize", "-G", action="store_true",
 options.add_argument("--chunker", "-c", action="store_true",
 	help="Read all the graphemes and create a chunking file.")
 
+options.add_argument("--phonotactics", "-t", action="store_true",
+	help="Read all the phonemes and create a phonotactics file.")
+
 makeset = options.add_argument_group()
 makeset.add_argument("--graphemic-context", "-g", default=0,
 	help="Use a graphemic context of the given number on each side. 1 = one grapheme on each side.")
@@ -62,9 +65,11 @@ if args.generalize:
 		#endfor
 		f.write(" }\n")
 	#endwith
-elif args.chunker:
-	with open(args.input, "r") as f: data = json.load(f, object_pairs_hook=odict)
+	sys.exit(0)
 
+
+with open(args.input, "r") as f: data = json.load(f, object_pairs_hook=odict)
+if args.chunker:
 	# onset: [grapheme, ...]
 	clustersByOnset = {}
 	onsetTotals, envTotals = {}, {}
@@ -111,66 +116,266 @@ elif args.chunker:
 		#endfor
 	#endfor
 
-	# Review them to find non-occurrences
-	# for k, v in iteritems(data):
-	# 	# Count how often this could have been a different cluster with the same onset.
-	# 	for i, (g, p) in enumerate(v):
-	# 		onset, probs = g[0], clusters[g][3]
-	# 		s = "".join([x for x, y in v[i:]])
-	# 		for x in clustersByOnset[onset]:
-	# 			if x != g and s.startswith(x):
-	# 				probs[x] = probs.get(x, 0) + 1
-	# 			#endif
-	# 		#endfor
-	# 	#endfor
-
-	# 	# Count numbers of spellings.
-	# 	lenk = len(k)
-	# 	for i in range(lenk):
-	# 		for j in range(i + 1, lenk + 1):
-	# 			bit = k[i:j]
-	# 			if bit in clusters: clusters[bit][4] += 1
-	# 		#endfor
-	# 	#endfor
-	# #endfor
-
-	# def scount(sp, envTotals=envTotals):
-	# 	if sp not in envTotals:
-	# 		envTotals[sp] = sum([x.count(sp) for x in iterkeys(data)])
-	# 	return envTotals[sp]
-	# #enddef
-
 	# Calculate percentages
 	for k, v in iteritems(clustersByOnset):
-		total = float(onsetTotals[k])
-		v = clustersByOnset[k] = list(v)
-		# for g in v:
-		# 	#occured = clusters[g][0]
-		# 	clusters[g][0] /= total
-
-		# 	context = clusters[g][1]
-		# 	for p, x in iteritems(context):
-		# 		try: context[p] /= float(scount(p + g))
-		# 		except ZeroDivisionError: context[p] = 0
-		# 	#endfor
-		# 	context = clusters[g][2]
-		# 	for n, x in iteritems(context):
-		# 		try: context[n] /= float(scount(g + n))
-		# 		except ZeroDivisionError: context[n] = 0
-		# 	#endfor
-		# 	context = clusters[g][3]
-		# 	for e, x in iteritems(context):
-		# 		try: context[e] /= float(scount(e[0] + g + e[1]))
-		# 		except ZeroDivisionError: context[e] = 0
-		# 	#endfor
-		# #endfor
+		clustersByOnset[k] = list(v)
 	#endfor
 
 	# Write out data
-	with open(args.output, "w") as f: json.dump([clustersByOnset, clusters, envTotals], f)
-else:
-	with open(args.input, "r") as f: data = json.load(f, object_pairs_hook=odict)
+	with open(args.output, "w") as f: json.dump([clustersByOnset, clusters, envTotals, onsetTotals], f)
+elif args.phonotactics:
+	# Constraints, if needed: https://en.wikipedia.org/wiki/English_phonology#Phonotactics
 
+	# grapheme: {("left"|"right"|"env"): { graph: total }, phoneme: {"total": #, ("left"|"right"|"env"): { graph: # } }
+	# phoneme: {"total": #, ("left"|"right"|"env"): { phone: # } }
+	g2p, pnn = {}, {}
+
+	# Retrieve all possible graphemes.
+	for v in itervalues(data):
+		lenv = len(v)
+		gp, stress, fv, lv = [], [], None, None
+		for i, (g, p) in enumerate(v):
+			if p[-1] in "012":
+				p, s = p[:-1], p[-1]
+				lv = i
+				if fv is None: fv = i
+			else: s = None
+			gp.append((g, p))
+			stress.append(s)
+		#endfor
+		for i, (g, p) in enumerate(gp):
+			if g in g2p:
+				tg = g2p[g]
+				tg["total"] += 1
+			else:
+				tg = g2p[g] = {
+					"total": 1,
+					"left": {}, "right": {}, "env": {},
+					"phones": {},
+				}
+			#endif
+
+			if p in tg["phones"]:
+				tgp = tg["phones"][p]
+				tgp["total"] += 1
+			else:
+				tgp = tg["phones"][p] = {
+					"total": 1,
+					"left": {}, "right": {}, "env": {},
+				}
+			#endif
+
+			prevG, prevP = v[i-1] if i > 0 else ("-", "-")
+			nextG, nextP = v[i+1] if i + 1 < lenv else ("-", "-")
+			envG, envP = prevG + "|" + nextG, prevP + "|" + nextP
+
+			tg["left"][prevG] = tg["left"].get(prevG, 0) + 1
+			tg["right"][nextG] = tg["right"].get(nextG, 0) + 1
+			tg["env"][envG] = tg["env"].get(envG, 0) + 1
+
+			tgp["left"][prevG] = tgp["left"].get(prevG, 0) + 1
+			tgp["right"][nextG] = tgp["right"].get(nextG, 0) + 1
+			tgp["env"][envG] = tgp["env"].get(envG, 0) + 1
+
+			mul = 2 if i > fv and i < lv and stress[i] else 1
+			if mul:
+				if p in pnn:
+					tp = pnn[p]
+					tp["total"] += mul
+				else:
+					tp = pnn[p] = {
+						"total": mul,
+						"left": {}, "right": {}, "env": {},
+					}
+				#endif
+
+				tp["left"][prevP] = tp["left"].get(prevP, 0) + mul
+				tp["right"][nextP] = tp["right"].get(nextP, 0) + mul
+				tp["env"][envP] = tp["env"].get(envP, 0) + mul
+		#endfor
+	#endfor
+
+	# Write out data
+	with open(args.output, "w") as f: json.dump([g2p, pnn], f)
+
+	# stops = ["P", "B", "T", "D", "K", "G"]
+	# affricates = ["CH", "JH"]
+	# fricatives = ["F", "V", "TH", "DH", "S", "Z", "SH", "ZH", "HH"]
+	# nasals = ["M", "N", "NG"]
+	# liquids = ["L", "R"]
+	# glides = ["Y", "W"] # also: semivowels
+	# vowels = ["AA", "AE", "AH", "AO", "AW", "AY", "EH", "ER", "EY", "IH", "IY", "OW", "OY", "UH", "UW", "Y", "W"]
+
+	# obstruents = stops + affricates + fricatives
+	# approximates = liquids + glides
+	# sonorants = nasals + approximates + vowels
+
+	# voiced = ["B", "D", "G", "JH", "V", "DH", "Z", "ZH"] + sonorants
+
+	# def l2r(l): return "(?:" + "|".join(l) + ")"
+
+	# # Valid onset forms:
+	# #  3C: S ([M T P K] Y | [T P K] R | [P K] L | K W | F R)
+	# #  2C: obstruent approximate | S [P T K M N F TH]
+	# #  1C: !NG
+	# onset = re.compile(
+	# 	"(?:S/(?:[MTPK]/Y|[TPK]/R|[PK]/L|K/W|F/R|[PTKMNF]|TH)"
+	# 	"|" + l2r(obstruents) + "/" + l2r(approximates) +
+	# 	"|(?!NG)[^/]+)$"
+	# )
+
+	# def findOnset(v, i):
+	# 	st = max(0, i - 3)
+	# 	vt = v[st : i]
+	# 	pj = "/".join([ph for sp, ph in vt])
+	# 	mo = onset.search(pj)
+	# 	if mo:
+	# 		idx = len(filter(lambda x: x, pj[0 : max(0, mo.start() - 1)].split("/")))
+	# 		# Also return if this is a shakey syllabification
+	# 		st += idx - 1
+	# 		return vt[idx:], st >= 0 and v[st][1] in vowels
+	# 	else:
+	# 		return [], False
+	# 	#endif
+	# #enddef
+
+	# # Maybe: Valid coda forms?
+
+	# g2p, phonemes = {}, {}
+
+	# for v in itervalues(data):
+	# 	lenv = len(v)
+
+	# 	nextOnset, nextHalf = None, None
+	# 	gp, stress = [], []
+	# 	for g, p in v:
+	# 		if p[-1] in "012": p, s = p[:-1], p[-1]
+	# 		else: s = None
+	# 		gp.append((g, p))
+	# 		stress.append(s)
+	# 	#endfor
+
+	# 	for i, (g, p) in enumerate(gp):
+	# 		# We want a sort of syllable identity. This is: [onset] nucleus [coda] or C*VC*
+	# 		# If only one is dropped from a syllable, it's more likely to be the coda that is.
+	# 		left, right, silentE = [], [], None
+	# 		if p in vowels:
+	# 			# Find consonants to the left (onset)
+	# 			if nextOnset:
+	# 				left, half = nextOnset, nextHalf
+	# 				nextOnset = None
+	# 			else: left, half = findOnset(v, i)
+
+	# 			# Find all consonants to the right (coda)
+	# 			foundVowel = False
+	# 			for j in range(i + 1, lenv):
+	# 				# Find the next vowel
+	# 				if v[j][:-1] in vowels:
+	# 					# and its onset
+	# 					nextOnset, nextHalf = findOnset(v, j)
+	# 					# then between i and this is the coda
+	# 					right = v[i+1:j-len(nextOnset)]
+	# 					foundVowel = True
+	# 					break
+	# 				#endif
+	# 			#endfor
+	# 			if not foundVowel:
+	# 				# Grab everything to the end, then
+	# 				right = v[i+1:]
+	# 			#endif
+
+	# 			value1 = 0.5 if half else 1
+	# 			value0 = 0.5 if half else 0
+
+	# 			# Store information about each part of the syllable
+	# 			for lg, lp in left:
+	# 				if lp not in phonemes:
+	# 					phonemes[lp] = {
+	# 						"onsetInit": 0, "onsetMedial": 0, "onsetFinal": 0, "onsetSimple": 0,
+	# 						"codaInit": 0, "codaMedial": 0, "codaFinal": 0, "codaSimple": 0,
+	# 						"onsets": {}, "codas": {}, "envs": {},
+	# 					}
+	# 				#endif
+
+	# 				# Add possible phonemes per grapheme
+	# 				if lg not in g2p: g2p[lg] = {}
+	# 				if lp in g2p[lg]:
+	# 					g2p[lg][lp][0] += value1
+	# 					g2p[lg][lp][2] += value0
+	# 				else: g2p[lg][lp] = [value1, 0, value0]
+	# 			#endfor
+
+	# 			# Nucleus
+	# 			cpj = "/".join([rp for rg, rp in right])
+	# 			if p not in phonemes: phonemes[p] = { "onsets": {}, "codas": {}, "envs": {} }
+	# 			phonemes[p]["codas"][cpj] = phonemes[p]["codas"].get(cpj, 0) + 1
+
+	# 			value2 = 1. / (len(left) + 1)
+
+	# 			def onen(ph):
+	# 				opj = "/".join([lp for lg, lp in ph])
+	# 				phonemes[p]["onsets"][opj] = phonemes[p]["onsets"].get(opj, 0) + value2
+	# 				env = opj + "|" + cpj
+	# 				phonemes[p]["envs"][env] = phonemes[p]["envs"].get(env, 0) + value2
+	# 			#enddef
+
+	# 			if half:
+	# 				for i in range(len(left)): onen(left[i:])
+	# 			else: onen(left)
+
+	# 			if g not in g2p: g2p[g] = {}
+	# 			if p in g2p[g]: g2p[g][p][1] += 1
+	# 			else: g2p[g][p] = [0, 1, 0]
+
+	# 			for rg, rp in right:
+	# 				if rp not in phonemes:
+	# 					phonemes[rp] = {
+	# 						"onsetInit": 0, "onsetMedial": 0, "onsetFinal": 0, "onsetSimple": 0,
+	# 						"codaInit": 0, "codaMedial": 0, "codaFinal": 0, "codaSimple": 0,
+	# 						"onsets": {}, "codas": {}, "envs": {},
+	# 					}
+	# 				#endif
+
+	# 				# Add possible phonemes per grapheme
+	# 				if rg not in g2p: g2p[rg] = {}
+	# 				if rp in g2p[rg]:
+	# 					g2p[rg][rp][0] += value0
+	# 					g2p[rg][rp][2] += value1
+	# 				else: g2p[rg][rp] = [value0, 0, value1]
+	# 			#endfor
+
+	# 			def onco(side, ph, value):
+	# 				lenl = len(ph)
+	# 				if lenl == 1:
+	# 					phonemes[ph[0][1]][side + "Simple"] += value
+	# 				elif lenl > 1:
+	# 					opj = "/".join([lp for lg, lp in ph])
+	# 					phonemes[ph[0][1]][side + "Init"] += value
+	# 					phonemes[ph[-1][1]][side + "Final"] += value
+	# 					for i, (lg, lp) in enumerate(ph):
+	# 						if i and i < lenl: phonemes[lp][side + "Medial"] += value
+	# 						if opj not in phonemes[lp][side + "s"]:
+	# 							phonemes[lp][side + "s"][opj] = value
+	# 						else: phonemes[lp][side + "s"][opj] += value
+	# 					#endfor
+	# 				#endif
+	# 			#enddef
+
+	# 			if half:
+	# 				for i in range(len(left) + 1):
+	# 					onco("coda", left[:i], value2)
+	# 					onco("onset", left[i:], value2)
+	# 				#endfor
+	# 			else: onco("onset", left, 1)
+
+	# 			onco("coda", right, 1)
+	# 		#endif
+	# 	#endfor
+	# #endfor
+	
+	# Write out data
+	#with open(args.output, "w") as f: json.dump([g2p, phonemes], f)
+else:
 	with open(args.output, "w") as f:
 		gc, pc = int(args.graphemic_context), int(args.phonetic_context)
 		stress, vi, se = args.stress, args.vowel_index, args.silent_e
--- trainer.py
+++ trainer.py
@@ -4,13 +4,17 @@
 # Copyright 2015 Sapphire Becker (logicplace.com)
 # MIT Licensed
 
-import timbl, timblapi, argparse, sys, json
+import timbl, timblapi
+import argparse, sys, json
 from future.utils import iteritems
 from collections import OrderedDict as odict
+import itertools
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--chunk", "-c", action="store_true", help="Only chunk graphemes")
-parser.add_argument("training", help="Training file name")
+parser.add_argument("--simple", "-s", action="store_true", help="Simple output")
+parser.add_argument("--morph", "-m", default=None, help="Morpheme training file")
+parser.add_argument("--chunking", "-c", help="Chunking file")
+parser.add_argument("--phonotactics", "-p", "-t", help="Phonotactics file")
 testing = parser.add_mutually_exclusive_group()
 testing.add_argument("-w", "--word", default=None, help="Test a word passed on the command line")
 testing.add_argument("testing", nargs="?", help="Testing file name")
@@ -24,142 +28,337 @@ else:
 	#endwith
 #endif
 
-if args.chunk:
-	with open(args.training, "r") as f:
-		gc, data, envs = tuple(json.load(f))
-		success, fail, problematic, untested = 0, 0, 0, 0
-		for t, rt in iteritems(test):
-			stest = "-" + t + "-"
-			dtest = dict(enumerate(stest))
-			ltest = len(stest)
+vowels = ["AA", "AE", "AH", "AO", "AW", "AY", "EH", "ER", "EY", "IH", "IY", "OW", "OY", "UH", "UW"]
 
-			def fetchPotential(i):
-				ret = []
-				for j in range(i + 1, ltest + 1):
-					g = stest[i:j]
-					if g in data:
-						p, n = dtest.get(i - 1, "-"), dtest.get(j, "-")
-						pg, gn, pgn = data[g][1].get(p, 0), data[g][2].get(n, 0), data[g][3].get(p+n, 0)
-						ps, sn, psn = envs.get(p+g, 0), envs.get(g+n, 0), envs.get(p+g+n, 0)
-						ret.append((g, float(pg + gn - pgn) / max(ps + sn - psn, 1)))
-					#endif
-				#endfor
-				return ret
-			#enddef
-
-			# P(<a> | "au") = <a>.next[u] / (<a>.next[u] + <au>.total)
-			# P(grapheme boundary between prev and graph) = sum([gg[x][1].get(prev, 0) for x in gc[graph[0]]])
-
-			def fitness(clusters):
-				# For now, try avg...
-				return float(sum(list(zip(*clusters))[1])) / len(clusters)
-			#enddef
-
-			def fork(clusters = [], start = 1):
-				for i in range(start, ltest - 1):
-					p = fetchPotential(i)
-					lp = len(p)
-
-					if lp == 0:
-						# ...?
-						print("Non-cluster...")
-					elif lp == 1:
-						# Only one option, can assume it's right.
-						clusters.append(p[0])
-					else:
-						# Fork each option and find the fittest.
-						mf, mc = 0, None
-						for j in p:
-							c = fork(clusters[:] + [j], i + len(j[0]))
-							f = fitness(c)
-							if f > mf: mf, mc = f, c
-						#endfor
-						return mc
-					#endif
-				#endfor
-				return clusters
-			#endfor
-			bestClusters = fork()
-
-			# Test or just print if there's no verification
-			if rt:
-				fit, goal = fitness(bestClusters), list(zip(*rt))[0]
-				if "_" in goal:
-					print("Problematic:")
-					problematic += 1
-					goal = [x  for x in goal if x != "_"]
+classifier = None
+if args.morph:
+	# i, limit = 0, 100
+	trainOn = []
+	doWeights = False
+	classifier = timbl.TimblClassifier("morpher", "-a 1 +D")
+	with open(args.morphs, "r") as f:
+		for l in f:
+			l = l.rstrip()
+			if l:
+				if l[0] != "#":
+					split = l.split(" ")
+					klass = split.pop()
+					classifier.append(split, klass)
+				# else:
+				# 	i += 1
+				# 	if i >= limit: break
 				#endif
-
-				if list(zip(*bestClusters))[0] == goal:
-					print("OK:", t, fit)
-					success += 1
-				else:
-					print("NG:", t, fit)
-					print("Found:", bestClusters)
-					print("Wanted:", goal)
-					fail += 1
-			else:
-				print(t, bestClusters, fitness(bestClusters))
-				untested += 1
 			#endif
 		#endfor
-		print("%i success; %i fail; %i untested; %i problematic" % (success, fail, problematic, untested))
 	#endwith
 
-	sys.exit(0)
+	classifier.train()
 #endif
 
-classifiers = []
+with open(args.chunking, "r") as f: gc, data, envs, ons = json.load(f)
+with open(args.phonotactics, "r") as f2: g2p, pnn = json.load(f2)
+gsuccess, gfail, problematic, untested = 0, 0, 0, 0
+psuccess, pfail, pbadg = 0, 0, 0
+for t, rt in iteritems(test):
+	stest = "-" + t + "-"
+	dtest = dict(enumerate(stest))
+	ltest = len(stest)
 
-# i, limit = 0, 100
-trainOn = []
-with open(args.training, "r") as f:
-	for l in f:
-		l = l.rstrip()
-		if l:
-			if l[0] != "#":
-				split = l.split(" ")
-				classes = []
-				for x in trainOn: classes.append(split.pop(x))
-				for i, c in enumerate(classifiers): c.append(split, classes[i])
-			elif l[0:5] == "#!w= ":
-				tmp = l[5:].split(" ")
-				with open("weights.txt", "w") as wf:
-					wf.write("# Fea.\tWeight")
-					for i, w in enumerate(tmp):
-						if w == "-": trainOn.append(i)
-						else: wf.write("\n%i\t%f" % (i - len(trainOn) + 1, float(w)))
-					#endfor
-				trainOn = trainOn[::-1]
-				classifiers = [timbl.TimblClassifier("g2p%s" % x, "-a 1 +D") for x in trainOn]
-			# else:
-			# 	i += 1
-			# 	if i >= limit: break
+	def fetchPotential(i):
+		ret = []
+		for j in range(i + 1, ltest + 1):
+			g = stest[i:j]
+			if g in data:
+				p, n = dtest.get(i - 1, "-"), dtest.get(j, "-")
+
+				pg, gn, pgn = data[g][1].get(p, 0), data[g][2].get(n, 0), data[g][3].get(p+n, 0)
+				ps, sn, psn = envs.get(p+g, 0), envs.get(g+n, 0), envs.get(p+g+n, 0)
+
+				#gOfOns, totalOns = data[g][0], ons[g[0]] 
+				ret.append((g, float(pg + gn - pgn) / max(ps + sn - psn, 1)))
 			#endif
-		#endif
-	#endfor
-#endwith
+		#endfor
+		return ret
+	#enddef
 
-for c in classifiers:
-	c.train()
-	c.api.getWeights("weights.txt", timblapi.Weighting.GR)
-#classifier.save()
+	# P(<a> | "au") = <a>.next[u] / (<a>.next[u] + <au>.total)
+	# P(grapheme boundary between prev and graph) = sum([gg[x][1].get(prev, 0) for x in gc[graph[0]]])
 
-# Test
-with open(args.testing, "r") as f:
-	for l in f:
-		l = l.rstrip()
-		if l and l[0] != "#":
-			features, goals = l.split(" "), []
-			for x in trainOn: goals.append(features.pop(x))
-			for i, c in enumerate(classifiers):
-				try: tmp = c.classify(features)
-				except timbl.ClassifyException as err:
-					tmp = err
-					print(err)
-					sys.exit(1)
-				print("Classifying '%s', should be %s:" % (" ".join(features), goals[i]), tmp)
+	def phitness(phones):
+		# Format: [(phone, P(// | left is <> u right is <>), P(// | left is // u right is //)), ...]
+		return float(sum([pg * pp for p, pg, pp in phones])) / len(phones)
+	#enddef
+
+	def phitest(clusters):
+		idxs = [0] * len(clusters)
+		opts = [list(g2p[g]["phones"].keys()) for g, _ in clusters]
+		dc = dict([(i, g) for i, (g, _) in enumerate(clusters)])
+
+		def gprob(x, i):
+			g = dc[i]
+
+			l, r = dc.get(i - 1, "-"), dc.get(i + 1, "-")
+
+			plg, prg, peg = g2p[g]["phones"][x]["left"].get(l, 0), g2p[g]["phones"][x]["right"].get(r, 0), g2p[g]["phones"][x]["env"].get(l + "|" + r, 0)
+			glg, grg, geg = g2p[g]["left"].get(l, 0), g2p[g]["right"].get(r, 0), g2p[g]["env"].get(l + "|" + r, 0)
+
+			return float(plg + prg - peg) / max(glg + grg - geg, 1)
+		#enddef
+
+		for i, o in enumerate(opts):
+			# Phoneme options for each chunk, sorted by probability given chunk environment.
+			opts[i] = sorted([(k, gprob(k, i)) for k in o], key = lambda x: -x[1])
+		#endfor
+
+		def ptotal(i, l, r):
+			# Calculate total for this option set based on options for this index
+			tlp, trp, tep = 0, 0, 0
+			for tph, tpr in opts[i]:
+				tlp += pnn[tph]["left"].get(l, 0)
+				trp += pnn[tph]["right"].get(r, 0)
+				tep += pnn[tph]["env"].get(l + "|" + r, 0)
 			#endfor
-		#endif
+
+			return tlp + trp - tep
+		#enddef
+
+		def pprob(l, ph, r, ttl):
+			plp, prp, pep = pnn[ph]["left"].get(l, 0), pnn[ph]["right"].get(r, 0), pnn[ph]["env"].get(l + "|" + r, 0)
+
+			tmp = float(plp + prp - pep)
+			if tmp == ttl == 0: return 0
+			elif ttl == 0: raise Exception("BAD TOTAL ERROR: top was %i" % tmp)
+			else: return tmp / ttl
+		#enddef
+
+		def ptprob(i, l, r):
+			mp, mf = None, 0
+			ttl = ptotal(i, l, r)
+			for ph, pr in opts[i]:
+				tf = pprob(l, ph, r, ttl)
+				#print(ph, pr, tf)
+				tf *= pr
+				if tf > mf: mp, mf = ph, tf
+			#endfor
+			return mp, mf
+		#enddef
+
+		# Find and fuzz middle vowels because fml
+		res, fv, lv = [], None, None
+		for i, o in enumerate(opts):
+			if o[0][0] in vowels:
+				lv = i
+				if fv is None: fv = i
+
+		for i, o in enumerate(opts):
+			# Take top for these.
+			if i <= fv or o[0][0] not in vowels or i >= lv: res.append(o[0])
+
+			# Middle vowels have trouble, do by phonemic environment
+			else: res.append(ptprob(i, opts[i-1][0][0], opts[i+1][0][0]))
+		#endfor
+
+		return [x for x, y in res], fitness(res)
+
+		# # Permutate options
+		# sureness = [(None, 0)] * 2
+		# cnt = 0
+		# dopts = dict(enumerate(opts))
+		# mp = []
+		# for i, v in enumerate(opts):
+		# 	# Find most probable given neighbor's options
+		# 	lside = [x for x, _ in dopts.get(i - 1, [("-", 1)])]
+		# 	rside = [x for x, _ in dopts.get(i + 1, [("-", 1)])]
+
+		# 	winners = {}
+		# 	for l, r in itertools.product(lside, rside):
+		# 		ttl = ptotal(i, l, r)
+		# 		mph, mpr = None, 0
+		# 		if ttl:
+		# 			for ph, _ in v:
+		# 				pr = pprob(l, ph, r, ttl)
+		# 				if pr > mpr: mph, mpr = ph, pr
+		# 			#endfor
+
+		# 		if mph is not None: winners[mph] = winners.get(mph, 0) + 1
+		# 	#endfor
+
+		# 	if len(winners) == 0: print(v)
+
+		# 	print(winners)
+		# 	winner = max(winners.items(), key=lambda x: x[1])[0]
+		# 	for ph, pr in v:
+		# 		if ph == winner:
+		# 			mp.append((ph, pr, winners[ph] / sum(winners.values())))
+		# 			break
+		# #endfor
+		# mf = phitness(mp)
+
+		# while True:
+		# 	unbroken = True
+		# 	#print("Idxs(%i)[%i]" % (cnt, len(idxs)), end=": ")
+		# 	for i, x in enumerate(idxs):
+		# 		idxs[i] = (x + 1) % len(opts[i])
+		# 		#print("%i +[%i] 1 = %i" % (x, len(opts[i]), idxs[i]), end=", ")
+		# 		if idxs[i] != 0:
+		# 			unbroken = False
+		# 			break
+		# 		#endif
+		# 	#endfor
+		# 	if unbroken: break
+
+		# 	#print("")
+
+		# 	cnt += 1
+
+		# 	probs = [v[idxs[i]] for i, v in enumerate(opts)]
+		# 	dprobs = dict(enumerate(probs))
+
+		# 	phpr = []
+		# 	for i, (ph, pr) in enumerate(probs):
+		# 		(l, lp), (r, rp) = dprobs.get(i - 1, ("-", 1)), dprobs.get(i + 1, ("-", 1))
+
+		# 		phpr.append(pprob(l, ph, r, ptotal(i, l, r)))
+		# 	#endfor
+
+		# 	probs = [(a, b, phpr[i]) for i, (a, b) in enumerate(probs)]
+		# 	probfit = phitness(probs)
+
+		# 	if probfit > sureness[-1][1]:
+		# 		sureness.pop(0)
+		# 		sureness.append((probs, probfit))
+		# 		print("\nNew best: ", probs, probfit)
+		# 	#endif
+		# #endwhile
+
+		# # Returns (most probable, surity, [other options])
+		# mp, mf = sureness.pop()
+		# return mp, mf, list(reversed(sureness))
+	#enddef
+
+	def fitness(clusters):
+		return float(sum([p for g, p in clusters])) / max(len(clusters), 1)
+	#enddef
+
+	def fork(clusters = [], start = 1):
+		for i in range(start, ltest - 1):
+			p = fetchPotential(i)
+			lp = len(p)
+
+			if lp == 0:
+				# ...?
+				print("Non-cluster...")
+			elif lp == 1:
+				# Only one option, can assume it's right.
+				clusters.append(p[0])
+			else:
+				# Fork each option and find the fittest.
+				mf, mc = 0, None
+				for j in p:
+					c = fork(clusters[:] + [j], i + len(j[0]))
+					f = 0 if phitest(c)[0].count(None) != 0 else fitness(c)
+					if f > mf: mf, mc = f, c
+				#endfor
+				if mc is not None: return mc
+			#endif
+		#endfor
+		return clusters
 	#endfor
-#endwith
+	bestClusters = fork()
+
+	# Test or just print if there's no verification
+	if rt:
+		fit, (ggoal, pgoal) = fitness(bestClusters), list(zip(*rt))
+		if "_" in goal:
+			print("Problematic:")
+			problematic += 1
+			goal = [x  for x in goal if x != "_"]
+		#endif
+
+		if [g for g, prb in bestClusters] == ggoal:
+			gsuccess += 1
+
+			pmp, pmf, opts = phitest(bestClusters)
+			if [p for p, pg, pp in pmp] == pgoal:
+				print("OK:", t, fit)
+				psuccess += 1
+			else:
+				print("Phones NG:", t, pmf)
+				pfail += 1
+			#endif
+		else:
+			print("Chunks NG:", t, fit)
+			print("Found:", bestClusters)
+			print("Wanted:", goal)
+			gfail += 1
+			pbadg += 1
+	else:
+		graphs, phones = [x for x,y in bestClusters], phitest(bestClusters)[0]
+
+		if not args.simple: print(t, bestClusters, fitness(bestClusters), phitest(bestClusters))
+
+		if args.morph:
+			def rmSilentE(gg):
+				if gg[-1][-1] == "e" and len(gg[-1]) > 1:
+					gg[-1] = gg[-1][:-1]
+			#enddef
+
+			# Keep going til we stop.
+			try:
+				while True:
+					cmd = classifier.classify((["-", "-"] + phones)[-3:])
+					if cmd == "<":
+						# Remove
+						if not args.simple: print("Popping", graphs[-1], phones[-1])
+						phones.pop()
+					elif cmd == ".":
+						# Stop
+						if not args.simple: print("Stopping, morphing")
+						rmSilentE(graphs)
+						break
+					else:
+						# Append
+						g, p = cmd.split("/")
+						if not args.simple: print("Appending", g, p)
+						rmSilentE(graphs)
+						graphs.append(g)
+						phones.append(p)
+					#endif
+				#endwhile
+			except timbl.ClassifyException as err:
+				print("WARNING: Failled classification at a point, assuming end.")
+			#endtry
+			graphs += ["i", "te"]
+			phones += ["AY", "T"]
+			print("".join(graphs))
+		elif args.simple: print(list(zip(graphs, phones)))
+		untested += 1
+	#endif
+#endfor
+if not args.simple:
+	print("Entries: %i problematic; %i untested" % (problematic, untested))
+	print("Chunks: %i success; %i fail" % (gsuccess, gfail))
+	print("Phones: %i success; %i fail (+%i bad chunking)" % (psuccess, pfail, pbadg))
+
+
+if not args.morph: sys.exit(0)
+
+
+# # Test
+# with open(args.testing, "r") as f:
+# 	for l in f:
+# 		l = l.rstrip()
+# 		if l and l[0] != "#":
+# 			features, goals = l.split(" "), []
+# 			for x in trainOn: goals.append(features.pop(x))
+# 			for i, c in enumerate(classifiers):
+# 				try: tmp = c.classify(features)
+# 				except timbl.ClassifyException as err:
+# 					tmp = err
+# 					print(err)
+# 					sys.exit(1)
+# 				print("Classifying '%s', should be %s:" % (" ".join(features), goals[i]), tmp)
+# 			#endfor
+# 		#endif
+# 	#endfor
+# #endwith
